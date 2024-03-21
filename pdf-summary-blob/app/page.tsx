770cd3288159a100e8e350e1d4f29a17f1ef047d
Nandhni G A <nandhni.25@gmail.com>
2024-03-21 14:20:42 +0530

    ui for front page

--- pdf-summary-blob/app/page.tsx
+++ pdf-summary-blob/app/page.tsx
@@ -2,15 +2,69 @@ import Form from "./ui/update-pdf-form";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="mt-4 flex grow flex-col gap-4">
-        <div className="items-center flex justify-center gap-6 rounded-lg bg-blue-400 px-6 py-10">
-          <p className={`items-center text-xl text-justify-center text-black-500 md:text-3xl md:leading-normal`}>
-            <strong>Unlocking Legal Complexity, Empowering your journey</strong> 
-          </p>
+    <>
+      <div className="container-xl bg-white h-screen p-2">
+        <div className="flex justify-between items-center md:justify-between md:items-center">
+          <img alt="logo" loading="lazy" width="100" height="100" decoding="async" data-nimg="1" className="color:transparent" src="/logo.webp" />
         </div>
-          <Form/>
+        <div className="w-full flex flex-col justify-center items-center md:mt-10">
+          <div className="flex flex-col justify-center items-center space-y-6">
+            <h1 className="lg:text-6xl font-serif p-4 text-black text-4xl text-center">
+              Legal Summaries Made Simple
+            </h1>
+            <p className="md:text-2xl font-sans text-center w-3/4 text-xl">
+              Leverage cutting-edge Artificial Intelligence to distill lengthy legal briefs, contracts, and documents into concise, clear summaries.
+            </p>
+            <div className="w-full">
+              <div className="flex flex-col items-center justify-center bg-white">
+                <div className="rounded-lg border bg-card text-card-foreground shadow-sm flex flex-col items-center justify-center w-3/5 h-48" style={{ border: '2px dashed black' }}>
+                  <div className="p-6 pt-0">
+                    <input type="file" accept="application/pdf" style={{ display: 'none' }} className="file-input" />
+                    <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline h-10 px-4 py-2">
+                      <div className="flex flex-col">
+                        <img alt="logo" loading="lazy" width="50" height="50" decoding="async" data-nimg="1" className="md:ml-32 ml-12" style={{ color: 'transparent' }} src="/file.webp" />
+                        <div className="overflow-hidden overflow-ellipsis whitespace-nowrap flex flex-col md:flex-row justify-center items-center">
+                          <p> Drag and Drop pdf </p>
+                          <p className="ml-1">or</p>
+                          <p className="ml-1">Click here to browse.</p><br />
+                        </div>
+                      </div>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center space-y-2 mt-5">
+          <div className="text-center text-base ml-4">
+            Wondering where to begin?
+          </div>
+          <div className="text-center text-3xl font-bold ml-4">
+            Try some Sample Judgements
+          </div>
+          <ul className="flex space-x-8 items-center justify-center">
+            <li className="hover:text-[#17A34A] hover:cursor-pointer hover:underline max-w-[30%] truncate">
+              <a href="/summary/f989be9a-6aed-49af-a174-eaed13423042/1">
+                <img alt="PDF icon" loading="lazy" width="48" height="48" decoding="async" data-nimg="1" style={{ color: 'transparent' }} src="pdf.svg" />
+                Sample File 1
+              </a>
+            </li>
+            <li className="hover:text-[#17A34A] hover:cursor-pointer hover:underline max-w-[30%] truncate">
+              <a href="/summary/2f52dc2d-9032-4d50-b440-5e59f9092861/1">
+                <img alt="PDF icon" loading="lazy" width="48" height="48" decoding="async" data-nimg="1" style={{ color: 'transparent' }} src="pdf.svg" />
+                Sample File 2
+              </a>
+            </li>
+            <li className="hover:text-[#17A34A] hover:cursor-pointer hover:underline max-w-[30%] truncate">
+              <a href="/summary/022adee0-1d90-4406-aeab-2300e3e7edad/1">
+                <img alt="PDF icon" loading="lazy" width="48" height="48" decoding="async" data-nimg="1" style={{ color: 'transparent' }} src="pdf.svg" />
+                Sample File 3
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </main>
+    </>
   );
 }
