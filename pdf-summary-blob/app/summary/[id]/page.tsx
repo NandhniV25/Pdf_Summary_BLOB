8918e22782be22510790488d95693dfbf74c71bf
Nandhni G A <nandhni.25@gmail.com>
2024-03-21 10:32:17 +0530

    redirect to next page display file id

--- pdf-summary-blob/app/summary/[id]/page.tsx
+++ pdf-summary-blob/app/summary/[id]/page.tsx
@@ -1,19 +1,7 @@
-import { fetchFileName } from "@/app/lib/action";
-
 export default async function Page({ params }: { params: { id: string }}) 
 {
-    const id = params.id;
-    const filename = await fetchFileName(id);   
-    var fileContentAfterDecode = new TextDecoder().decode(filename.file_content);
-    console.log(id + " - "+id);
-    console.log(id + " - "+filename.file_name);
-    console.log(id + " - "+fileContentAfterDecode);
-    return (<div><div>
-        {filename.file_name}
-    </div>
-    <div>
-        {fileContentAfterDecode}
-    </div>
+    return (<div>
+        {params.id}
     </div>
     );
 }
