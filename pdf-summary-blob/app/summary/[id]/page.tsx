46fcbbd529500968134ae047c70c1ec89c8ae46a
Nandhni G A <nandhni.25@gmail.com>
2024-03-21 21:27:00 +0530

    display pdf content in next page

--- pdf-summary-blob/app/summary/[id]/page.tsx
+++ pdf-summary-blob/app/summary/[id]/page.tsx
@@ -1,7 +1,12 @@
-export default async function Page({ params }: { params: { id: string }}) 
-{
-    return (<div>
-        {params.id}
-    </div>
+export default async function Page({ params }: { params: { id: string } }) {
+    const link = "https://hpacaornrjmmex3z.public.blob.vercel-storage.com/"
+    const viewParams = "#toolbar=0&navpanes=0&scrollbar=0"
+    return (<div className="container-xl bg-white h-screen p-2">
+        <iframe
+            src={link + params.id + viewParams}
+            height="100%"
+            width="50%"
+        ></iframe>
+    </div >
     );
 }
